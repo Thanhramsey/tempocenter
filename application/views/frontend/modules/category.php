9dc6293b3f3a0ea2fa2b57004930768587fa3728
thanh <thanhwilshere96@gmail.com>
2023-04-17 17:00:20 +0700

    test

--- application/views/frontend/modules/category.php
+++ application/views/frontend/modules/category.php
@@ -5,12 +5,12 @@
 
 <?php
 $listcat = $this->Mcategory->category_menu(0);
-$html_menu='<ul class="main-ul">';
+$html_menu='<ul class="main-ul list-thanh">';
 foreach ($listcat as $menu) {
     $parentid = $menu['id'];
     $submenu = $this->Mcategory->category_menu($parentid);
-    $html_menu.='<li>';
-    $html_menu.="<a href='san-pham/".$menu['link']." ' title=' ".$menu['name']."'>";
+    $html_menu.='<li class="nav-item dropdown">';
+    $html_menu.="<a  class='dropdown-toggle' href='san-pham/".$menu['link']." ' title=' ".$menu['name']."'>";
     $html_menu.=$menu['name'];
     if(!empty($submenu)){
         $html_menu.='<i class="fa fa-angle-right pull-right" aria-hidden="true">';
@@ -19,7 +19,7 @@ foreach ($listcat as $menu) {
     $html_menu.="</a>";
     if(count($submenu))
     {
-        $html_menu.='<ul class="sub">';
+        $html_menu.='<ul class="sub dropdown-menu fade-down">';
         foreach ($submenu as $menu1){
             $html_menu.='<li>';
             $html_menu.="<a href='san-pham/".$menu1['link']." ' title=' ".$menu1['name']." '> ".$menu1['name']."</a>";
